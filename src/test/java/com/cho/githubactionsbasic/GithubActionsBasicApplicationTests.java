e0ed213f312edc6e6b20fc0be5d84c775e01ba25
0802222 <0802222@naver.com>
2026-03-23 20:37:05 +0900

    first commit

---
+++ src/test/java/com/cho/githubactionsbasic/GithubActionsBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cho.githubactionsbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
